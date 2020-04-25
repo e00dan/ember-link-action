6cf0823ac6f28efe72e4e266d6a5c207cf3f295a
Daniel <neverkuzi@gmail.com>
2020-04-25 21:44:39 +0200

    chore: update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,14 @@
+'use strict';
+
 module.exports = {
   root: true,
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
+    ecmaFeatures: {
+      legacyDecorators: true
+    }
   },
   plugins: [
     'ember'
@@ -15,6 +21,8 @@ module.exports = {
     browser: true
   },
   rules: {
+    'ember/no-jquery': 'error',
+    'ember/no-new-mixins': 'off'
   },
   overrides: [
     // node files
