41280f1945faaaadec6d9c670c4c347149002afa
Daniel Kmak <neverkuzi@gmail.com>
2020-10-31 18:12:55 +0100

    fix: update ember-try config

--- config/ember-try.js
+++ config/ember-try.js
@@ -6,18 +6,10 @@ module.exports = async function() {
   return {
     scenarios: [
       {
-        name: 'ember-lts-3.12',
+        name: 'ember-lts-3.20',
         npm: {
           devDependencies: {
-            'ember-source': '~3.12.0'
-          }
-        }
-      },
-      {
-        name: 'ember-lts-3.16',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.16.0'
+            'ember-source': '~3.20.5'
           }
         }
       },
@@ -30,41 +22,10 @@ module.exports = async function() {
         }
       },
       {
-        name: 'ember-beta',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('beta')
-          }
-        }
-      },
-      {
         name: 'ember-canary',
         npm: {
           devDependencies: {
             'ember-source': await getChannelURL('canary')
-          }
-        }
-      },
-      // The default `.travis.yml` runs this scenario via `npm test`,
-      // not via `ember try`. It's still included here so that running
-      // `ember try:each` manually or from a customized CI config will run it
-      // along with all the other scenarios.
-      {
-        name: 'ember-default',
-        npm: {
-          devDependencies: {}
-        }
-      },
-      {
-        name: 'ember-default-with-jquery',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true
-          })
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^0.5.1'
           }
         }
       },
