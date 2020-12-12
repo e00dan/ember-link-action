351327e8f781ac80fb60c47185dc2e76637188e5
Daniel Kmak <neverkuzi@gmail.com>
2020-12-12 18:31:02 +0100

    chore: bump embroider

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -15,5 +15,5 @@ module.exports = function(defaults) {
   */
 
  const { maybeEmbroider } = require('@embroider/test-setup');
- return maybeEmbroider(app);
+ return maybeEmbroider(app, { skipBabel: [{ package: 'base-64' }] });
 };
