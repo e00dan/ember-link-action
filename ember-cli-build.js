b396504224f0a0660672c89b92eebb155ae52578
Daniel Kmak <neverkuzi@gmail.com>
2020-12-13 13:26:16 +0100

    chore: remove IE11 support

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -15,5 +15,5 @@ module.exports = function(defaults) {
   */
 
  const { maybeEmbroider } = require('@embroider/test-setup');
- return maybeEmbroider(app, { skipBabel: [{ package: 'base-64' }] });
+ return maybeEmbroider(app);
 };
